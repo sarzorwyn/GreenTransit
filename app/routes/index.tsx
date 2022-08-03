70795e0d7df18e545a1d7a02537445b12c17f44a
Ivan <ivan.t.mailbox@gmail.com>
2022-08-04 01:33:45 +0800

    Added some support for mobile

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -13,15 +13,15 @@ export default function Index() {
     <div className="bg-gray-50 flex h-screen justify-center items-center">
       <img src="/images/winter-train.jpg" alt="" className="w-full h-full object-cover blur-md bg-black/30"></img> 
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between absolute rounded-3xl shadow-lg bg-blue-100">
-        <h2>
-          <h3 className=" text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl"> 
+        <div>
+          <div className=" text-4xl font-bold tracking-tight text-gray-900 sm:text-4xl"> 
             <span className="block text-gray-700">Welcome to </span>
             <span className="text-green-500">green</span><span className="text-violet-500">Transit</span>
-          </h3>
+          </div>
           <div className="text-3xl  block font-light lg:py-2 text-gray-500">
             Start your <span className="text-green-500"> green journey</span> today.
           </div>
-        </h2>
+        </div>
         <div className="mt-8 flex lg:mt-0 ml-12 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
             <a href="maps" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 transform transition-all hover:scale-105"> Let's Go! </a>
--- app/routes/maps.tsx
+++ app/routes/maps.tsx
@@ -1,6 +1,6 @@
 import { Autocomplete, DirectionsRenderer, GoogleMap, Marker, useJsApiLoader, useLoadScript } from "@react-google-maps/api";
 import { ActionFunction, LoaderArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import { useEffect, useRef, useState } from "react";
 
 export async function loader({ request }: LoaderArgs) {
@@ -10,8 +10,13 @@ export async function loader({ request }: LoaderArgs) {
 declare type Libraries = ("drawing" | "geometry" | "localContext" | "places" | "visualization")[];
 export const libraries:Libraries = ['places']
 
+/**
+ * The main map function with overlays.
+ * TODO: All routes using different transportation options will be calculated and toggled when the user chooses them.
+ * TODO: Make the url metadata change when a route is calculated so that the user can share/revisit the output
+ * @returns the map and overlays
+ */
 export default function Maps() {
-    
     const apiKey = useLoaderData();
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: apiKey,
@@ -19,22 +24,31 @@ export default function Maps() {
     });
     const [map, setMap] = useState<google.maps.Map>();
     const [directionsResponse, setDirectionsResponse] = useState<google.maps.DirectionsResult>();
+    // const [searchParams, setSearchParams] = useSearchParams();
     const [distance, setDistance] = useState('');
     const [duration, setDuration] = useState('');
-    const center = { lat: 1.295972, lng: 103.851986 }
 
     const startRef = useRef<HTMLInputElement>(null);
     const endRef = useRef<HTMLInputElement>(null);
-    let bounds : google.maps.LatLngBounds | undefined;
+    
+    const center = { lat: 1.295972, lng: 103.851986 }
+
+    let bounds: google.maps.LatLngBounds | undefined;
+    useEffect(() => {
+        if (map !== undefined) {
+            bounds = map.getBounds();
+            console.log(bounds)
+        }
+    }, [map])
+    
+
     const calcBounds = () => {
         if (map === undefined) {
             return;
         }
-        const bound = map.getBounds();
-        console.log(bound)
-        const ne = bound!.getNorthEast(); // LatLng of the north-east corner
-        const sw = bound!.getSouthWest(); // LatLng of the south-west corder
-        bounds = bound;
+        const timer = setTimeout(() => {bounds = map.getBounds();
+            }, 3000)
+        return () => clearTimeout(timer)
     }
 
     const calculateRoute = async () => {
@@ -64,7 +78,7 @@ export default function Maps() {
     }
     
 
-    if (!isLoaded) {
+    if (!isLoaded ) {
         return <div/>;
     }
 
@@ -84,33 +98,15 @@ export default function Maps() {
                 onLoad={m => setMap(m)}
                 onBoundsChanged={calcBounds}
             >
-                {(
+                {(directionsResponse &&
                     <DirectionsRenderer directions={directionsResponse} />
                 )}
             </GoogleMap>
         </div>
-        {/* <div className="z-10 max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between absolute rounded-3xl shadow-lg text-2xl bg-blue-100">
-            <div className="relative z-0 mb-6 w-full group">
-                <Autocomplete>
-                    <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                </Autocomplete>
-                <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
-            </div>
-            
-                <Autocomplete>
-                    <input name="start" placeholder="Choose starting point" id="start" ref={startRef} className=" mx-10"/>
-                </Autocomplete>
-                <Autocomplete>
-                    <input name="end" placeholder="Choose end point" id="end" ref={endRef} />
-                </Autocomplete>
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-                <span>Distance: {distance}</span>
-            
-        </div> */}
-        <form className="z-1 max-w-7xl mx-auto py-3 px-4 sm:px-6  absolute rounded-3xl shadow-lg text-xl bg-blue-100">
-            <div className="lg:py-15 lg:px-8 lg:flex lg:items-start lg:justify-between lg:space-x-1">
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+        <Form className="z-1 flex-grow w-screen flex-col absolute py-1 px-2 shadow-lg text-xl bg-gray-200 sm:flex-row sm:w-auto sm:px-3 sm:rounded-b-3xl">
+            <div className="border-separate md:py-1 md:px-8 md:flex md:items-start md:justify-between md:space-x-1">
+                <div className="mb-4 md:mr-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="origin">
                         Origin
                     </label>
                     <Autocomplete bounds={bounds}>
@@ -118,23 +114,20 @@ export default function Maps() {
                     </Autocomplete>
                 </div>
                 <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="destination">
                         Destination
                     </label>
-                    <Autocomplete>
-                        <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="End Point" type="text" placeholder="End Point" ref={endRef}/>
+                    <Autocomplete bounds={bounds}>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="End Point" type="text" placeholder="End Point" ref={endRef}/>
                     </Autocomplete>
                 </div>
             </div>
-            <div className="flex items-center justify-between pt-12">
-                <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-                    Forgot Password?
-                </a>
+            <div className="flex items-center justify-between sm:flex-row">
                 <button className="bg-green-600 hover:bg-green-700 text-white font-medium py-1 px-2 mr-auto rounded focus:outline-none focus:shadow-outline" type="button" onClick={calculateRoute}>
                     Calculate
                 </button>
             </div>
-        </form>
+        </Form>
     </div>
     );
 }
