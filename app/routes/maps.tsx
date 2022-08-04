faab40d4853b68f6e9e21ebca25df68e2777b8ea
Ivan <ivan.t.mailbox@gmail.com>
2022-08-04 12:27:06 +0800

    limit autocomplete to singapore and some uiscaling

--- app/routes/maps.tsx
+++ app/routes/maps.tsx
@@ -31,25 +31,27 @@ export default function Maps() {
     const startRef = useRef<HTMLInputElement>(null);
     const endRef = useRef<HTMLInputElement>(null);
     
-    const center = { lat: 1.295972, lng: 103.851986 }
+    const center = { lat: 1.361534, lng: 103.815990 }
 
-    let bounds: google.maps.LatLngBounds | undefined;
-    useEffect(() => {
-        if (map !== undefined) {
-            bounds = map.getBounds();
-            console.log(bounds)
-        }
-    }, [map])
+    const restrictions:google.maps.places.ComponentRestrictions | undefined = {country:'sg'}; // I can't afford a worldwide search for the api :(
+    
+    // let bounds: google.maps.LatLngBounds | undefined;
+    // useEffect(() => {
+    //     if (map !== undefined) {
+    //         bounds = map.getBounds();
+    //         console.log(bounds)
+    //     }
+    // }, [map])
     
 
-    const calcBounds = () => {
-        if (map === undefined) {
-            return;
-        }
-        const timer = setTimeout(() => {bounds = map.getBounds();
-            }, 3000)
-        return () => clearTimeout(timer)
-    }
+    // const calcBounds = () => {
+    //     if (map === undefined) {
+    //         return;
+    //     }
+    //     const timer = setTimeout(() => {bounds = map.getBounds();
+    //         }, 3000)
+    //     return () => clearTimeout(timer)
+    // }
 
     const calculateRoute = async () => {
         if (startRef.current === null || startRef.current.value === '' || endRef.current === null || endRef.current.value === '') {
@@ -90,40 +92,40 @@ export default function Maps() {
                 zoom={12}
                 mapContainerStyle={{ width: '100%', height: '100%', zIndex: 0}}
                 options={{
+                clickableIcons: false,
                 zoomControl: true,
                 streetViewControl: false,
                 mapTypeControl: false,
-                fullscreenControl: false,
+                fullscreenControl: false
                 }}
                 onLoad={m => setMap(m)}
-                onBoundsChanged={calcBounds}
             >
                 {(directionsResponse &&
                     <DirectionsRenderer directions={directionsResponse} />
                 )}
             </GoogleMap>
         </div>
-        <Form className="z-1 flex-grow w-screen flex-col absolute py-1 px-2 shadow-lg text-xl bg-gray-200 sm:flex-row sm:w-auto sm:px-3 sm:rounded-b-3xl">
-            <div className="border-separate md:py-1 md:px-8 md:flex md:items-start md:justify-between md:space-x-1">
-                <div className="mb-4 md:mr-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="origin">
+        <Form className="z-1 flex-grow w-screen flex-col absolute px-2 shadow-lg text-xl bg-gray-200 sm:flex-row sm:w-auto sm:py-1 sm:px-3 sm:rounded-b-3xl">
+            <div className="border-separate mb-1 sm:px-4 sm:flex sm:items-start sm:justify-between sm:space-x-1 md:mb-4">
+                <div className="md:mr-4">
+                    <label className="block text-gray-700 text-sm font-bold sm:mb-2" htmlFor="origin">
                         Origin
                     </label>
-                    <Autocomplete bounds={bounds}>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Start Point" type="text" placeholder="Start Point" ref={startRef}/>
+                    <Autocomplete restrictions={restrictions}>
+                        <input className="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Start Point" type="text" placeholder="Start Point" ref={startRef}/>
                     </Autocomplete>
                 </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="destination">
+                <div className="">
+                    <label className="block text-gray-700 text-sm font-bold sm:mb-2" htmlFor="destination">
                         Destination
                     </label>
-                    <Autocomplete bounds={bounds}>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="End Point" type="text" placeholder="End Point" ref={endRef}/>
+                    <Autocomplete restrictions={restrictions}>
+                        <input className="shadow appearance-none border rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="End Point" type="text" placeholder="End Point" ref={endRef}/>
                     </Autocomplete>
                 </div>
             </div>
             <div className="flex items-center justify-between sm:flex-row">
-                <button className="bg-green-600 hover:bg-green-700 text-white font-medium py-1 px-2 mr-auto rounded focus:outline-none focus:shadow-outline" type="button" onClick={calculateRoute}>
+                <button className="bg-green-600 hover:bg-green-700 text-white font-medium py-1 px-2 mb-2 ml-auto sm:mr-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={calculateRoute}>
                     Calculate
                 </button>
             </div>
